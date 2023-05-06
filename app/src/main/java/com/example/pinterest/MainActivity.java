dc2f0577aa2efc8f5715a17d884b759c46e816a3
vidaNr <vida_200243@yahoo.com>
2023-05-06 11:30:37 +0330

    Pinterest android project

---
+++ app/src/main/java/com/example/pinterest/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.pinterest;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+
+
+
+}
---
+++ app/src/main/java/com/example/pinterest/splashActivity.java
@@ -0,0 +1,35 @@
+package com.example.pinterest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class splashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+
+
+        Handler handler =new Handler();
+        Runnable runnable= new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(splashActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        };
+
+        handler.postDelayed(runnable, 1500);
+
+    }
+}
