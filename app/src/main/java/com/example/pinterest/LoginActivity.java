10e860397f02f0a27c6096686df5fd7c579168f3
vidaNr <vida_200243@yahoo.com>
2023-05-11 23:51:32 +0330

    WIP on main

--- app/src/main/java/com/example/pinterest/LoginActivity.java
+++ app/src/main/java/com/example/pinterest/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.pinterest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -20,15 +22,31 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
         ImageView ivShowPass = findViewById(R.id.iv_show_pass);
         EditText etShowPass = findViewById(R.id.et_show_pass);
+
+
         ivShowPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                ivShowPass.setImageResource(!ShowPass ? R.drawable.ic_eye_off : R.drawable.ic_eye_on);
-                etShowPass.setInputType(ShowPass ?InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD : InputType.TYPE_CLASS_TEXT);
+                ivShowPass.setImageResource(!ShowPass ? R.drawable.ic_eye_on : R.drawable.ic_eye_off);
+                etShowPass.setInputType(ShowPass ? InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD : InputType.TYPE_CLASS_TEXT);
 
                 ShowPass = !ShowPass;
             }
         });
+
+
+        Button btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("aaaa", "ljcds");
+                startActivity( new Intent(LoginActivity.this , MainActivity.class));
+                finish();
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/pinterest/MainActivity.java
+++ app/src/main/java/com/example/pinterest/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.pinterest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-
+    }
 }
