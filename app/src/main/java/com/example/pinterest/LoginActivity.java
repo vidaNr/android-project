ad7bccbc2b60ea1976bc042d90571dd7303d48aa
vidaNr <vida_200243@yahoo.com>
2023-05-31 09:55:53 +0330

    validation email and password

--- app/src/main/java/com/example/pinterest/LoginActivity.java
+++ app/src/main/java/com/example/pinterest/LoginActivity.java
@@ -12,69 +12,81 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import androidx.annotation.Nullable;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class LoginActivity extends Activity {
 
     boolean ShowPass = false;
+    private TextInputEditText ietEmail;
+    private TextInputEditText ietPass;
+    Pattern pattern;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ImageView ivShowPass = findViewById(R.id.iv_show_pass);
-        EditText etShowPass = findViewById(R.id.et_show_pass);
 
 
-        ivShowPass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ivShowPass.setImageResource(ShowPass ? R.drawable.ic_eye_off : R.drawable.ic_eye_on);
-                etShowPass.setInputType(ShowPass ? InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD : InputType.TYPE_CLASS_TEXT);
-
-                ShowPass = !ShowPass;
-
-            }
-        });
+        ietEmail = findViewById(R.id.iet_email);
+        ietPass = findViewById(R.id.iet_show_pass);
 
 
         Button btnSignIn = findViewById(R.id.btn_sign_in);
         btnSignIn.setOnClickListener(view -> {
 
-            EditText etEmail= findViewById(R.id.et_email);
-            EditText etPass= findViewById(R.id.et_show_pass);
 
-
-            if((etEmail.getText().toString().isEmpty())){
+            if ((ietEmail.getText().toString().isEmpty())) {
                 Toast.makeText(this, "Email is required!", Toast.LENGTH_SHORT).show();
-
-                return;
-            }
-
-            if((etPass.getText().toString().isEmpty())){
+            } else if ((ietPass.getText().toString().isEmpty())) {
                 Toast.makeText(this, "Password is required!", Toast.LENGTH_SHORT).show();
-                return;
             }
 
-
-            goMain();
+            validation();
 
         });
     }
-        private void goMain(){
 
+    private void validation() {
 
-            SharedPreferences pref = getSharedPreferences("pinterest", MODE_PRIVATE);
-            SharedPreferences.Editor editor = pref.edit();
-            editor.putBoolean("is_login", true);
-            editor.apply();
+        String email = "example@example.com";
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n";
 
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish();
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
 
+        if (matcher.matches()) {
+            // Email is valid
+            Toast.makeText(LoginActivity.this , "Email is valid",Toast.LENGTH_SHORT).show();
+            goMain();
+
+        } else {
+            // Email is invalid
+            Toast.makeText(LoginActivity.this , "Email is invalid",Toast.LENGTH_SHORT).show();
+            return;
         }
 
+    }
+
+    private void goMain() {
+
+        SharedPreferences pref = getSharedPreferences("pinterest", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("is_login", true);
+        editor.apply();
+
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+
+    }
+
 }
+
 
 
